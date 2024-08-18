d5176f0582f0061eb34891f18873328c142a4150
FabianKel <drk.fab@gmail.com>
2024-08-18 17:01:54 -0600

    Better TP func

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,11 @@
+
 use minifb::Key;
 use minifb::Window;
+use std::collections::HashMap;
+use std::f32::consts::PI;
+
+use crate::framebuffer::Framebuffer;
+use crate::caster::cast_ray;
 
 use crate::player::Player;
 
@@ -9,11 +15,65 @@ pub fn process_events(
     maze: &Vec<Vec<char>>,
     block_size: usize,
     previous_mouse_x: &mut f32,
+    special_positions: &HashMap<char, Vec<(usize, usize)>>,
+    framebuffer: &mut Framebuffer,
 ) {
     let move_speed = 10.0;
     let collision_margin = 5.0;
     let rotate_speed = 0.2;
 
+    let angle = player.a;
+
+    // Realizar un cast de rayos para detectar intersección con un bloque
+    let intersect = cast_ray(framebuffer, maze, player, angle, block_size, false);
+
+    // Verificar si el jugador está mirando un bloque especial y si está a una distancia cercana
+    if intersect.distance < 50.0 && (intersect.impact == '(' || intersect.impact == ')' || intersect.impact == '[' || intersect.impact == ']') {
+        println!("Press E");
+
+        // Si se presiona 'E', realizar el teletransporte
+        if window.is_key_down(Key::E) {
+            match intersect.impact {
+                '(' => {
+                    if let Some(target_positions) = special_positions.get(&')') {
+                        if let Some(&(tx, ty)) = target_positions.get(0) {
+                            player.pos.x = (tx * block_size) as f32 - 25.0;
+                            player.pos.y = (ty * block_size) as f32 + 25.0;
+                            player.a = 45.0;
+                        }
+                    }
+                }
+                ')' => {
+                    if let Some(target_positions) = special_positions.get(&'(') {
+                        if let Some(&(tx, ty)) = target_positions.get(0) {
+                            player.pos.x = (tx * block_size) as f32 - 25.0;
+                            player.pos.y = (ty * block_size) as f32 + 25.0;
+                            player.a = 45.0;
+                        }
+                    }
+                }
+                '[' => {
+                    if let Some(target_positions) = special_positions.get(&']') {
+                        if let Some(&(tx, ty)) = target_positions.get(0) {
+                            player.pos.x = (tx * block_size) as f32 - 25.0;
+                            player.pos.y = (ty * block_size) as f32 + 25.0;
+                            player.a = 45.0;
+                        }
+                    }
+                }
+                ']' => {
+                    if let Some(target_positions) = special_positions.get(&'[') {
+                        if let Some(&(tx, ty)) = target_positions.get(0) {
+                            player.pos.x = (tx * block_size) as f32 - 25.0;
+                            player.pos.y = (ty * block_size) as f32 + 25.0;
+                            player.a = 45.0;
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
     // Calcula la nueva posición propuesta hacia adelante
     if window.is_key_down(Key::W) || window.is_key_down(Key::Up) {
         let new_x = player.pos.x + player.a.cos() * move_speed;
